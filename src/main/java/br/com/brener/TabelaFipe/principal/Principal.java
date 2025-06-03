daa9d8ffd7e76ea2f91ab958609aee8567d1b0c8
Brener Oliveira Brito <=>
2025-06-03 20:34:33 -0300

    Funcionalidades com mais interações com o usuário e filtragem da API

--- src/main/java/br/com/brener/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/brener/TabelaFipe/principal/Principal.java
@@ -1,10 +1,16 @@
 package br.com.brener.TabelaFipe.principal;
 
 import br.com.brener.TabelaFipe.model.Dados;
+import br.com.brener.TabelaFipe.model.Modelos;
+import br.com.brener.TabelaFipe.model.Veiculo;
 import br.com.brener.TabelaFipe.service.ConsumoApi;
 import br.com.brener.TabelaFipe.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -27,9 +33,9 @@ public class Principal {
         var opcao = leitura.nextLine();
         String endereco;
 
-        if (opcao.toLowerCase().contains("carr") | opcao.toLowerCase().contains("car")) {
+        if (opcao.toLowerCase().contains("carr")) {
             endereco = URL_BASE + "carros/marcas";
-        } else if ((opcao.toLowerCase().contains("mot")) | opcao.toUpperCase().contains("mo")) {
+        } else if (opcao.toLowerCase().contains("mot")) {
             endereco = URL_BASE + "motos/marcas";
         } else {
             endereco = URL_BASE + "caminhoes/marcas";
@@ -38,6 +44,49 @@ public class Principal {
         var json = consumo.obterDados(endereco);
         System.out.println(json);
         var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para consulta: ");
+        var opcaoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + opcaoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("\nDigite um trecho do nome do carro a ser buscado: ");
+        var nomeVeiculo = leitura.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                    .collect(Collectors.toList());
+
+        System.out.println("\nModelos filtrados");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("\nDigite o código do modelo para buscar os valores de avaliação: ");
+        var codigoModelo = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumo.obterDados(endereco);
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < anos.size(); i++) {
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = consumo.obterDados(enderecoAnos);
+            Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("\nTodos os veículos filtrados com avaliações por ano: ");
+        veiculos.forEach(System.out::println);
 
     }
 }
